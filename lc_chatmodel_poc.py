f1f5932433760462de5c260a92d173e9c955201f
singh-anshuman <anshumansingh137@gmail.com>
2025-01-10 18:38:01 +0530

    Added prompt template instead of adding messages directly

--- lc_chatmodel_poc.py
+++ lc_chatmodel_poc.py
@@ -1,17 +1,27 @@
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
-
+from langchain_core.prompts import ChatPromptTemplate
 
 def call_open_ai():
     model = ChatOpenAI(model="gpt-4o-mini")
-    messages = [
-        SystemMessage("Translate the following from English into Marathi"),
-        HumanMessage("Hey there, this is Anshuman Singh. How are you doing ?"),
-    ]
+    # messages = [
+    #     SystemMessage("Translate the following from English into German"),
+    #     HumanMessage("Hi, Good Morning. This is Anshuman Singh. How are you doing ?"),
+    # ]
 
-    output = model.invoke(messages)
-    print(output.content)
+    system_template = "Translate the following from English into {language}"
+
+    prompt_template = ChatPromptTemplate.from_messages(
+        [("system", system_template), ("user", "{text}")]
+    )
+
+    prompt = prompt_template.invoke({"language": "Italian", "text": "Hey there !"})
+
+    # output = model.invoke(prompt_template)
+    for token in model.stream(prompt):
+        print(token.content, end="")
+    # print(output.content)
 
 
 if __name__ == "__main__":
